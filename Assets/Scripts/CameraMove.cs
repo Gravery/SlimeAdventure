38926abdd037339aa45a1e2d48db3cea38caf099
Guilherme Calça <guilhermecalca@hotmail.com>
2022-11-29 14:57:20 -0300

    Transição de Camera

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject Camera;
+    public float x;
+    public float y;
+
+
+    private float targetX, targetY;
+
+    void Start () {
+
+    Camera = GameObject.Find("Main Camera");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+    if(other.CompareTag("Player")) {
+        targetX = Camera.transform.position.x + x;
+        targetY = Camera.transform.position.y + y;
+        Camera.transform.position = new Vector3(targetX, targetY, Camera.transform.position.z);
+        x = -x;
+        y = -y;
+    
+    }
+    }
+}
